a425c3099b768077cedb94322f536cc42b31215c
Mark Soulier <mark.soulier@usu.edu>
2025-03-13 13:45:22 -0600

    new onboarding modal in front dev

--- src/utils/ApolloProvider.tsx
+++ src/utils/ApolloProvider.tsx
@@ -97,7 +97,7 @@ const AuthApolloProvider = ({
 
 	const authLink = createAuthLink(getValidToken);
 	const httpLink = createHttpLink({
-		uri: "http://localhost:4001/graphql",
+		uri: "http://localhost:4000/graphql",
 	});
 
 	const client = new ApolloClient({
